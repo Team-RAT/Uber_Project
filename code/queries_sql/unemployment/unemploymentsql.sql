36fd4a69427a1e235c48052b7b7f37161819ec85
KG-TRAINING33\Student <tabithadavies@tiscali.co.uk>
2017-11-23 10:19:14 +0000

    sql query for unemployment rate table

--- code/queries_sql/unemployment/unemploymentsql.sql
+++ code/queries_sql/unemployment/unemploymentsql.sql
@@ -1,21 +1,20 @@
 USE [uber_project]
 GO
 
-SELECT 
-	   [UALAD]
-      ,[Code]
-      ,[Jan 11 to Dec 11 Rate (%)] AS [2011 Rate (%)]
-      ,[Jan 11 to Dec 11  + - (%)2] AS [2011 +/- (%)]
-      ,[Jan 2012 to Dec 2012 Rate (%)] AS [2012 Rate (%)]
-      ,[Jan 2012 to Dec 2012 + - (%)2] AS [2012 +/- (%)]
-      ,[Jan 2013 to Dec 2013 Rate (%)] AS [2013 Rate (%)]
-      ,[Jan 2013 to Dec 2013 + - (%)2] AS [2013 +/- (%)]
-      ,[Jan 2014 to Dec 2014 Rate (%)] AS [2014 Rate (%)]
-      ,[Jan 2014 to Dec 2014 + - (%)2] AS [2014 +/- (%)]
-      ,[Jan 2015 to Dec 2015 Rate (%)] AS [2015 Rate (%)]
-      ,[Jan 2015 to Dec 2015 + - (%)2] AS [2015 +/- (%)]
-      ,[Jan 2016 to Dec 2016  Rate (%)] AS [2016 Rate (%)]
-      ,[Jan 2016 to Dec 2016  + - (%)2] AS [2016 +/- (%)]
-FROM [dbo].[UnemploymentData]
+CREATE SCHEMA unemployment
 GO
 
+CREATE TABLE unemployment.Rates_perc
+Go
+
+SELECT 
+	   [UALAD]
+      ,[Jan 11 to Dec 11 Rate (%)] AS [2011]
+      ,[Jan 2012 to Dec 2012 Rate (%)] AS [2012]
+      ,[Jan 2013 to Dec 2013 Rate (%)] AS [2013]
+      ,[Jan 2014 to Dec 2014 Rate (%)] AS [2014]
+      ,[Jan 2015 to Dec 2015 Rate (%)] AS [2015]
+      ,[Jan 2016 to Dec 2016  Rate (%)] AS [2016]
+INTO unemployment.Rates_perc
+FROM [dbo].[UnemploymentData]
+GO
